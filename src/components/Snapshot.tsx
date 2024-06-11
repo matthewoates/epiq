b85dfd89d15b80a2c1f1b7b95973dce828f1d93c
Matt Oates <matt@coachmatt.io>
2024-06-10 19:46:01 -0700

    snapshot enhancements

--- src/components/Snapshot.tsx
+++ src/components/Snapshot.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { type CSSProperties, type ReactNode } from "react";
 import Constants from "../Constants";
 
 type SnapshotProps = {
@@ -6,17 +6,21 @@ type SnapshotProps = {
   img: string;
   overlay?: ReactNode;
   selected: boolean;
+  style?: CSSProperties;
 };
 
 const BOX_SHADOW = '0 0 10px 10px #0f09';
 
-function Snapshot({ onClick, img, overlay, selected }: SnapshotProps) {
+function Snapshot({ onClick, img, overlay, selected, style }: SnapshotProps) {
   return (
     <div
       style={{
         ...Constants.imgSize,
+        imageRendering: 'pixelated',
         backgroundImage: `url("${img}")`,
-        boxShadow: selected ? BOX_SHADOW : 'none'
+        backgroundSize: '100%',
+        boxShadow: selected ? BOX_SHADOW : 'none',
+        ...style
       }}
       onClick={onClick}
     >
